fc4e8c9d965ae5241d67d90b9241b4dc2694fe92
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-08-03 01:58:39 +0300

    Update DiscordMessagingService.php

--- app/Services/MessagingServices/DiscordMessagingService.php
+++ app/Services/MessagingServices/DiscordMessagingService.php
@@ -94,22 +94,28 @@ class DiscordMessagingService
      */
     public function redirect(string $state)
     {
-        dd([
-            'client_id'     => adminSetting('chats.discord.client_id'),
-            'redirect_uri'  => $this->redirectUri(),
-            'response_type' => 'code',
-            'scope'         => 'bot',
-            'permissions'   => '8',
-            'state'         => $state,
-        ]);
-        $url = 'https://discord.com/oauth2/authorize?' . http_build_query([
-            'client_id'     => adminSetting('chats.discord.client_id'),
-            'redirect_uri'  => $this->redirectUri(),
-            'response_type' => 'code',
-            'scope'         => 'bot',
-            'permissions'   => '8',
-            'state'         => $state,
-        ]);
+        $scopes = [
+            'identify', 'guilds', 'gdm.join', 'rpc.notifications.read', 
+            'rpc.video.read', 'rpc.screenshare.write', 'messages.read', 
+            'applications.store.update', 'openid', 'applications.commands.permissions.update', 
+            'email', 'guilds.join', 'bot', 'rpc.voice.read', 
+            'rpc.video.write', 'rpc.activities.write', 'applications.builds.read', 
+            'applications.entitlements', 'connections', 'guilds.members.read', 
+            'rpc', 'rpc.voice.write', 'rpc.screenshare.read', 
+            'webhook.incoming', 'applications.commands', 'role_connections.write'
+        ];
+
+        $queryParams = [
+            'client_id'        => adminSetting('chats.discord.client_id'),
+            'permissions'      => '8',
+            'response_type'    => 'code',
+            'redirect_uri'     => $this->redirectUri(),
+            'integration_type' => '0',
+            'scope'            => implode(' ', $scopes),
+            'state'            => $state,
+        ];
+
+        $url = 'https://discord.com/oauth2/authorize?' . http_build_query($queryParams);
 
         return Redirect::away($url);
     }
