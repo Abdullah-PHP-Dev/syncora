70f530629ac174658eec85ff869bf44e2c4c4284
Abdullah-m5azn <abdullah.dev.analyst@gmail.com>
2026-08-04 23:21:36 +0300

    Update PostAccountController.php

--- app/Http/Controllers/Admin/PostAccountController.php
+++ app/Http/Controllers/Admin/PostAccountController.php
@@ -754,7 +754,7 @@ class PostAccountController extends Controller
 
         $profileResponse = $api->request('get', 'https://open.tiktokapis.com/v2/user/info/', [
             'Authorization' => 'Bearer ' . $token['access_token'],
-        ], ['fields' => 'open_id,display_name,avatar_url,username']);
+        ], ['fields' => 'open_id,display_name,avatar_url,username_description']);
 
         $profile = $profileResponse->successful() ? ($profileResponse->json()['data']['user'] ?? []) : [];
     dd($profile);
