25a7a314c6efd8b3947b6f527629d19959776371
Abdullah-m5azn <abdullah.dev.analyst@gmail.com>
2026-08-13 00:21:11 +0300

    Update InstagramMessengerService.php

--- app/Services/MessagingServices/InstagramMessengerService.php
+++ app/Services/MessagingServices/InstagramMessengerService.php
@@ -109,9 +109,7 @@ class InstagramMessengerService
     {
         $version = adminSetting('messaging.instagram.graph_version') ?: (adminSetting('messaging.meta.graph_version') ?: 'v21.0');
 
-$response = Http::withHeaders([
-    'Authorization' => 'Bearer IGAAqPVIo94cFBZAFpMVXo3eEtOSkVkczFkeC1ERU4wMjJxTXgzejJsQng2THJ5VTN5UUw1Vi14SjFCcnFzandvUVBWeE9xemwzQWlrY3FnNFVPYVMzX1JidURfVThuTWt3SjRPNGZAYSE1rT2xlNE9YUHVR',
-])->get("https://graph.facebook.com/v26.0/1098590715835617");
+$response = Http::withToken('IGAAqPVIo94cFBZAFpMVXo3eEtOSkVkczFkeC1ERU4wMjJxTXgzejJsQng2THJ5VTN5UUw1Vi14SjFCcnFzandvUVBWeE9xemwzQWlrY3FnNFVPYVMzX1JidURfVThuTWt3SjRPNGZAYSE1rT2xlNE9YUHVR')->get("https://graph.facebook.com/v26.0/1098590715835617");
 
         // if (!$result['success']) {
         //     return [];
