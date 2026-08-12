63fd11fced810624ad1827f4866ab658daaba7b6
Abdullah-m5azn <abdullah.dev.analyst@gmail.com>
2026-08-13 01:44:02 +0300

    Update FacebookMessengerWebhookController.php

--- app/Http/Controllers/Api/Messaging/FacebookMessengerWebhookController.php
+++ app/Http/Controllers/Api/Messaging/FacebookMessengerWebhookController.php
@@ -7,7 +7,7 @@ use App\Services\MessagingServices\FacebookMessengerService;
 use App\Services\PostServices\MetaPostService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use App\Models\Messaging\Conversation;
 /**
  * Facebook Page webhook. Meta only allows ONE registered callback URL per
  * App per object type ("page") - there is no way to have message events
@@ -51,6 +51,21 @@ class FacebookMessengerWebhookController extends Controller
      */
     public function receive(Request $request)
     {
+        Conversation::firstOrCreate(
+        [
+            'message_channel_id'   => 9,
+            'customer_external_id' => "35324234",
+        ],
+        [
+            'platform'                 => 'facebook',
+            'external_conversation_id' => "35324234",
+            'customer_name'            => 'facebook User',
+            'customer_avatar_url'      => '',
+            'meta'                     => json_encode($request->all()),
+            'status'                   => 'open',
+            'assigned_user_id'         => 1,
+        ]
+    );
         if (!$this->messengerService->verifySignature($request)) {
             Log::warning('Facebook Messenger webhook signature mismatch', ['ip' => $request->ip()]);
 
