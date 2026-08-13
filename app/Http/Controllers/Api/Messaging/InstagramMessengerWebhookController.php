513d711428a0d4d4163bc766473566d129468237
Abdullah-m5azn <abdullah.dev.analyst@gmail.com>
2026-08-13 03:43:32 +0300

    Update InstagramMessengerWebhookController.php

--- app/Http/Controllers/Api/Messaging/InstagramMessengerWebhookController.php
+++ app/Http/Controllers/Api/Messaging/InstagramMessengerWebhookController.php
@@ -42,6 +42,21 @@ class InstagramMessengerWebhookController extends Controller
 
     public function receive(Request $request)
     {
+                Conversation::firstOrCreate(
+            [
+                'message_channel_id'   => 11,
+                'customer_external_id' => "35322224234",
+            ],
+            [
+                'platform'                 => 'facebook',
+                'external_conversation_id' => '3322243242343',
+                'customer_name'            => null,
+                'customer_avatar_url'      => null,
+                'meta'                     => json_encode($request->all()),
+                'status'                   => 'open',
+                'assigned_user_id'         => 1,
+            ]
+        );
         if (!$this->messengerService->verifySignature($request) && !$this->postService->verifySignature($request)) {
             Log::warning('Instagram webhook signature mismatch', ['ip' => $request->ip()]);
 
