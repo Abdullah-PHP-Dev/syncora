4e66d79c89c3e00878a945e52c08d734cb14e4ae
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-08-17 01:48:03 +0300

    rear

--- app/Http/Controllers/Api/Messaging/DiscordWebhookController.php
+++ app/Http/Controllers/Api/Messaging/DiscordWebhookController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\PostComment;
 
-class DiscordMessengerWebhookController extends Controller
+class DiscordWebhookController extends Controller
 {
     public function __construct(
         protected DiscordMessagingService $discordService,
