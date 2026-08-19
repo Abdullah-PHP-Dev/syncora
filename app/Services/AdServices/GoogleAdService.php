1ae87b5cab7ae3ba6c872ed936c1a51b7e1907af
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-08-19 23:12:16 +0300

    Update GoogleAdService.php

--- app/Services/AdServices/GoogleAdService.php
+++ app/Services/AdServices/GoogleAdService.php
@@ -154,9 +154,9 @@ class GoogleAdService
         ];
 
         $campaign = array_merge($campaign, $this->biddingStrategyPayload($request));
-
+        
         $result = $this->mutate($endpoint, ['operations' => [['create' => $campaign]]]);
-
+        dd($result, $endpoint, ['operations' => [['create' => $campaign]]]);
         if (!$result['success']) {
             return $result;
         }
