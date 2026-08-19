f0cedaa8a9cbfce04a588df26ff272a411f64b41
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-08-19 11:50:07 +0300

    Update GoogleAdsApiTrait.php

--- app/Services/AdServices/Concerns/GoogleAdsApiTrait.php
+++ app/Services/AdServices/Concerns/GoogleAdsApiTrait.php
@@ -332,7 +332,7 @@ trait GoogleAdsApiTrait
     protected function mutate($endpoint, array $payload)
     {
         $response = $this->apiService->post($endpoint, $this->header, $payload);
-
+        dd($response);
         if (!$response['success']) {
             return $this->errorResponse($response['data']['error']['message'] ?? 'Google Ads API request failed.');
         }
