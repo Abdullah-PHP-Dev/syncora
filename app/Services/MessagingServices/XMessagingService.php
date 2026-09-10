074ea6811960432dd527a7b5090d6f99a110b07f
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-09-11 01:46:13 +0300

    Update XMessagingService.php

--- app/Services/MessagingServices/XMessagingService.php
+++ app/Services/MessagingServices/XMessagingService.php
@@ -292,7 +292,7 @@ class XMessagingService
     private function registerWebhookIfNeeded(): ?string
     {
         $existingId = adminSetting('messaging.x.webhook_id');
-     
+        dd($existingId);
         if ($existingId) {
             return $existingId;
         }
