26c767968c3629c8725d2eeab653ba1fcb609fc0
Abdullah-m5azn <abdullah.khan@m5azn.sa>
2026-09-11 02:05:53 +0300

    Update XMessagingService.php

--- app/Services/MessagingServices/XMessagingService.php
+++ app/Services/MessagingServices/XMessagingService.php
@@ -328,7 +328,7 @@ class XMessagingService
         'https://api.x.com/2/webhooks',
         ['Authorization' => "Bearer {$bearerToken}"]
     );
-        dd($createResponse, $response, $webhookUrl);
+        dd($createResponse, $response, $webhookUrl, $bearerToken);
         if (!$createResponse['success']) {
             Log::warning('X Account Activity webhook registration failed.', ['body' => $createResponse['data'] ?? null]);
 
